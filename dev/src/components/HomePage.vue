4fe7d56e1c62c0c8e03505f4c05e33aba8143ae8
austin030606 <austin030606@gmail.com>
2022-09-23 17:48:38 +0800

    feat: homepage basic layout

--- dev/src/components/HomePage.vue
+++ dev/src/components/HomePage.vue
@@ -1,12 +1,52 @@
 <template>
   <div class="pageWrap">
     <div class="pageBody">
-      homepage
+      <TitleCard class="blocks" />
+      <div class="blocks" style="width: max-content">
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+        <p>This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. This is a place holder for testing. </p>
+      </div>
+      <Footer class="blocks" />
+      <!--
+        recent posts
+        music tier list
+        movie tier list
+        footer
+      -->
     </div>
   </div>
 </template>
 
-<script setup>
+<script>
+import TitleCard from './TitleCard.vue';
+import Footer from './Footer.vue';
+
+export default {
+    components: {
+      TitleCard,
+      Footer
+    },
+    data () {
+        return {
+            
+        }
+    }
+}
 </script>
 
 <style scoped>
@@ -14,11 +54,20 @@
     width: 100%;
     max-width: 1440px;
     padding-top: 3.5rem;
+    overflow: hidden;
 }
 
 .pageWrap {
     display: flex;
     justify-content: center; 
     align-items: center;
+}
+
+.blocks {
+    margin-top: 4rem;
+    margin-bottom: 4rem;
+    display: block; 
+    margin-left: auto; 
+    margin-right: auto;
 }
 </style>
---
+++ dev/src/components/TitleCard.vue
@@ -0,0 +1,60 @@
+<template>
+    <div style="width: max-content">
+        <div class="titlecard">
+            <div class="text">
+                <p>
+                    Hi, I'm Austin.
+                </p>
+                <p>
+                    I'm a university student studying computer science at <a style="color: #26BEE8; font-weight: bold;" href="https://www.cs.nycu.edu.tw/" target="_blank">NYCU</a>.
+                </p>
+                <p>
+                    You have reached my personal blog where I write about stuff. (´･Д･)」
+                </p>
+            </div>
+            <div class="img">
+                <img src="/public/meme.jpg" style="width: inherit"/>
+                <p style="margin: 0;">
+                    哈哈笑死這根本就是我
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+    },
+    data () {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+.titlecard {
+    width: max-content;
+    display: flex;
+    justify-content: center; 
+    align-items: center;
+}
+
+.text {
+    font-size: 2rem;
+    font-weight: 500;
+    margin: 0;
+    width: 36rem;
+    margin-right: 6rem;
+}
+
+.img {
+    width: 20rem;
+    height: 17rem;
+}
+
+p {
+    margin: 0.8rem;
+}
+</style>
